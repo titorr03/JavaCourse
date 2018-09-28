40b60d1bcab2df65c1a590e8d1030b78cc68061e
pench <pench@DESKTOP-I4PA44V>
2018-09-28 11:01:03 +0300

    dfg

---
+++ Java/src/masivi2.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class masivi2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in =new Scanner(System.in);
+        int n = in.nextInt();
+        int [] arr= new int[n];
+        
+        for(int i=0;i<arr.length;i++){
+            arr[i] = in.nextInt();
+        }
+        int nm= arr[0]+arr[1];
+        
+        for(int r=0;r<arr.length;r++){
+        
+            for(int t=0;t<arr.length;t++){
+                if(r==t){
+                    
+                }else if(arr[r]+arr[t]<nm){
+                    nm=arr[r]+arr[t];
+                }
+                
+            }
+        }
+        
+        
+        System.out.println(nm);
+	}
+
+}
