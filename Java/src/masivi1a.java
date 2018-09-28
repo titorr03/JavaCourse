4d193bc5356fdd3f1760c0f520ee20475e87302e
pench <pench@DESKTOP-I4PA44V>
2018-09-28 10:59:09 +0300

    a

---
+++ Java/src/masivi1a.java
@@ -0,0 +1,53 @@
+import java.util.*;
+public class masivi1a {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner (System.in);
+		int n = input.nextInt();
+		int [] arr= new int[n];
+		for(int i=0; i<arr.length; i++) {
+			arr[i]=input.nextInt();
+		}
+		float sredno=0;
+		for(int a=0; a <arr.length; a++) {
+			sredno=sredno+arr[a];
+		}
+		sredno=Math.round(sredno/n);
+		int c=0;
+		float raz;
+		int raz2=0;;
+		raz=Math.abs(arr[0]-sredno);
+		for(int r=0; r <arr.length; r++) {
+			if (Math.abs(arr[r]-sredno)<raz) {
+				raz=Math.abs(arr[r]-sredno);
+				raz2= arr[r];
+				c++;
+			}
+}
+		if(c==0) {
+			raz2=arr[0];
+			System.out.println(raz2);
+		}else{
+			
+			for(int u=0;u <arr.length;u++) {
+			if (Math.abs(arr[u]-sredno)==raz){
+				if (raz2>arr[u]){
+					System.out.println(arr[u]);
+				break;
+			}else {
+				System.out.println(raz2);
+				break;
+				}
+			}
+		}
+		
+		}
+		
+		
+		
+		
+		
+	}
+
+}
