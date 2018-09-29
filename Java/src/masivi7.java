8015a7e82ef3775a81719bd82fc5adda9e131855
pench <pench@DESKTOP-I4PA44V>
2018-09-29 14:12:14 +0300

    7 zadacha

---
+++ Java/src/masivi7.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class masivi7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in=new Scanner(System.in);
+		int n=in.nextInt();
+		int [] arr= new int[n];
+		for(int i=0;i<arr.length;i++) {
+			arr[i]=in.nextInt();
+		}
+		int k=in.nextInt();
+		int[] arr2= new int[k];
+		for(int i=0;i<arr2.length;i++) {
+			arr2[i]=in.nextInt();
+		}
+		
+		int nk=0;
+		
+		for(int i=0;i<arr2.length;i++) {
+			for(int j=0;j<arr.length;j++) {
+				if(arr[j]==arr2[i]) {
+					nk++;
+				}
+			}
+		}
+		
+		if(nk==k) {
+			System.out.println("suvpadat");
+		}
+		else{
+			System.out.println("ne suvpadat");
+		}
+		
+		
+
+	}
+
+}
