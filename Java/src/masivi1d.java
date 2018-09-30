2ad8e2328b8137e9835f460f7228017fdf173005
pench <pench@DESKTOP-I4PA44V>
2018-09-30 18:05:59 +0300

    1 zadacha D podtochka

--- Java/src/masivi1d.java
+++ Java/src/masivi1d.java
@@ -4,26 +4,36 @@ public class masivi1d {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner in = new Scanner(System.in);
+		boolean vtoro=true;
         int n= in.nextInt();
         int [] arr = new int[n];
         for(int i=0;i<arr.length;i++){
             arr[i]=in.nextInt();
         }
+        
         int ng=arr[0];
         for (int i=0;i<arr.length;i++){
             if(arr[i]>ng){
                 ng=arr[i];
             }
         }
-        
+        int nm=arr[0];
+        for (int i=0;i<arr.length;i++){
+            if(arr[i]<nm){
+                nm=arr[i];
+            }
+        }
+        if(ng==nm) {
+        	vtoro=false;
+        }
   
         
         for(int i=0; i<arr.length;i++){
             if(arr[i]==ng){
-                arr[i]=-2147483648;
+                arr[i]=nm-1;
             }
         }
-        ng=-2147483648;
+        ng=nm-1;
         int vng=arr[0];
         for(int i=0;i<arr.length;i++){
             if(arr[i]>vng){
@@ -32,7 +42,11 @@ public class masivi1d {
             }
         }
         
-        System.out.println(vng);
+        if(vtoro==false) {
+        	System.out.println("nqma vtoro");
+        }else {
+        	System.out.println(vng);
+        }
         
 		
 		
